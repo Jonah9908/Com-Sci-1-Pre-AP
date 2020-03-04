43fb3b01908400a4e2c14ec2c7fb95ae2c0062d6
Jonah Sims <jonahmsims@gmail.com>
2020-03-04 08:53:48 -0600

    new labs

---
+++ New folder/LASTNAME_EvenOdd.py
@@ -0,0 +1,5 @@
+# First Last
+# EvenOdd.py
+
+numbers = [11, 43, 52, 68, 110, 111, 120, 15, 4, 926]
+
---
+++ New folder/LASTNAME_Triangles.py
@@ -0,0 +1,7 @@
+# First Last
+# Triangles.py
+
+sides1 = [5, 6, 7, 8, 9]
+sides2 = [5, 6, 8, 8, 10]
+sides3 = [5, 5, 9, 8, 9]
+
