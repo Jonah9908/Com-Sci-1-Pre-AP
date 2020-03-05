15d8051e1c7e63b6c56a02c0b41b80896854148c
Jonah S <jonahmsims@gmail.com>
2020-03-04 20:21:52 -0600

    Finished

--- Problem Set2.5/Sims_Triangles.py
+++ Problem Set2.5/Sims_Triangles.py
@@ -7,5 +7,27 @@ sides3 = [5, 5, 9, 8, 9]
 
 import math
 
-#todo all the other stuff
+
+# Calculates area
+def area(side1, side2, side3):
+    s = (side1 + side2 + side3) / 2
+    a = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+    return a
+
+
+# Determines what type of triangle it is
+def triangle_type(side1, side2, side3):
+    if side1 == side2 and side1 == side3 and side2 == side3:
+        return "equilateral triangle"
+    elif side1 != side2 and side1 != side3 and side2 != side3:
+        return "scalene triangle"
+    else:
+        return "isosceles triangle"
+
+
+# Final print statement/ script
+spot = 0
+for len in sides1:
+    print(triangle_type(sides1[spot], sides2[spot], sides3[spot]) + " with area %.2f" % area(sides1[spot], sides2[spot], sides3[spot]))
+    spot += 1
 
