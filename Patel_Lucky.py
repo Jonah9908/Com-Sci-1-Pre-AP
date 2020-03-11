b091acf090b3fdf75b9637de135716488da46134
Ninjam06 <icloudmili@gmail.com>
2020-03-11 15:31:20 -0500

    EvenOdd Lab

---
+++ Patel_Lucky.py
@@ -0,0 +1,28 @@
+# Milan Patel
+# Lucky.py
+
+
+numbers = [58264, 24864, 125689, 121212353545]
+
+
+def sum_digits(number):
+    num = str(number)
+    spot = 0
+    add = 0
+    while spot < len(num):
+        add += int(num[spot])
+        spot += 1
+    return add
+
+
+
+
+spot_outer = 0
+for items in numbers:
+    number = numbers[spot_outer]
+    counter = (sum_digits(numbers[spot_outer]))
+    spot_outer += 1
+    if counter % 2 == 0:
+        print("Lucky", number)
+    else:
+        print("NOT Lucky", number)
---
+++ Patel_Pixels.py
@@ -0,0 +1,34 @@
+# Milan Patel
+# Pixels.py
+
+
+line1 = "BBBBBBBBBB"
+line2 = "BBNNBBNNBB"
+line3 = "BBNNBBNNBB"
+line4 = "BBBBBBBBBB"
+line5 = "BBBBNNBBBB"
+line6 = "BBNBBBBNBB"
+line7 = "BBBNNNNBBB"
+line8 = "BBBBBBBBBB"
+
+def b_count(line):
+    spot = 0
+    count = 0
+    for letters in line:
+        if line[spot] == "B":
+            count += 1
+            spot += 1
+        else:
+            spot += 1
+    return  count
+
+
+total = b_count(line1) + b_count(line2) + b_count(line3) + b_count(line4) + b_count(line5) + b_count(line6) + b_count(
+    line7) + b_count(line8)
+
+combined = line1 + line2 + line3 + line4 + line5 + line6 + line7 + line8
+
+
+percent = total / len(combined) * 100
+number = "%.1f" % percent
+print(number, "%")
