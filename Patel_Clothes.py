efe34d58da0ca755b7c9e92900fe002d21982bf7
Ninjam06 <icloudmili@gmail.com>
2020-03-11 18:58:01 -0500

    Clothes Lab

--- Patel_Clothes.py
+++ Patel_Clothes.py
@@ -2,8 +2,8 @@
 # Patel_Clothes.py  
 
 
-
-clothes = clothes = ["P khaki", "S blueish green", "S apple", "K bright",  "P blue", "P red", "K purple",  "S ugly", "K stupid", "P big red"]
+clothes = ["P khaki", "S blueish green", "S apple", "K bright", "P blue", "P red", "K purple", "S ugly", "K stupid",
+           "P big red"]
 spot = 0
 pants = []
 shirts = []
@@ -15,34 +15,31 @@ item = clothes[length]
 def substring(sentence, start, end):
     word = ""
     spot = start
-    while(spot < end):
+    while spot < end:
         word += sentence[spot]
         spot += 1
     return word
 
 
 for item in clothes:
-    description = substring(item, 2, len(item)) 
-    if(item[0] == "S"):
-        shirts.append(description) 
-    elif(item[0] == "P"):
+    description = substring(item, 2, len(item))
+    if item[0] == "S":
+        shirts.append(description)
+    elif item[0] == "P":
         pants.append(description)
-    elif(item[0] == "K"):
+    elif item[0] == "K":
         socks.append(description)
     spot += 1
 
 L = len(shirts)
 
-if(len(pants) < L):
+if len(pants) < L:
     L = len(pants)
-    
-if(len(socks) < L):
-    L = len(socks)
 
+if len(socks) < L:
+    L = len(socks)
 
 spot = 0
 while spot < L:
-    print (shirts[len(shirts) - spot - 1] + ", " + pants[len(pants) - spot - 1] + ", " + socks[len(socks) - spot - 1])
+    print(shirts[len(shirts) - spot - 1] + ", " + pants[len(pants) - spot - 1] + ", " + socks[len(socks) - spot - 1])
     spot += 1
-
-
