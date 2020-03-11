b4e63a5f5308deebf0074c6769a5b88dc9d61a96
Ninjam06 <icloudmili@gmail.com>
2020-03-11 18:53:59 -0500

    Prime Century Lab

--- Patel_PrimeCentury.py
+++ Patel_PrimeCentury.py
@@ -1,14 +1,20 @@
 # Milan Patel
 # PrimeCentury.py
 
+
 def is_prime(year):
     prime = True
     num = year / 2
     count = 2
     while count < num:
         if year % count == 0:
-            return "composite"
+            prime = False
         count += 1
-    return "prime"
+    return prime
 
-print(is_prime(2003))
+
+spot = 2000
+for num in range(2000, 2100):
+    if is_prime(spot):
+        print(spot)
+    spot += 1
